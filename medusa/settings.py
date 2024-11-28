871dbc76097d76d35c944394c312525b76722dae
simonfroggatt <simon@safetysignsandnotices.co.uk>
2024-11-28 22:36:45 +0400

    cdn serialser

--- medusa/settings.py
+++ medusa/settings.py
@@ -33,7 +33,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0%g862#abjugdfjbz1i3y3oc0)ifrdds-!t_m&-^c(4#!d8y#%'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
