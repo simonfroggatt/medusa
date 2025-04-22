9052449b868cda37f7f8dc464528028328ff5dd4
simonfroggatt <simon@safetysignsandnotices.co.uk>
2025-04-22 15:26:58 +0400

    product edit

--- apps/orders/views.py
+++ apps/orders/views.py
@@ -48,6 +48,8 @@ from cryptography.fernet import Fernet
 import re
 
 from ..symbols.models import OcTsgSymbols
+import logging
+logger = logging.getLogger('apps')
 
 
 class Orders2(viewsets.ViewSet):
@@ -574,13 +576,16 @@ def order_product_edit(request, order_id, order_product_id):
 
     if request.method == 'POST':
         form = ProductEditForm(request.POST, instance=order_product)
+        logger.debug(f"order_product_edit = {form}")
         if form.is_valid():
+            logger.debug(f"form is valid")
             data['form_is_valid'] = True
             order_product.save()
             calculate_order_total(order_id)
             #now see if we need to update any options / variants
             update_product_options_and_variant_options(request.POST, order_id, order_product_id, order_product.product_id)
         else:
+            logger.debug(f"form not valid = {form.errors}")
             data['form_is_valid'] = False
 
     else:
