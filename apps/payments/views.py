d898b1b66a823c1af1645b931a629b1850625382
simonfroggatt <simon@safetysignsandnotices.co.uk>
2025-05-06 17:53:45 +0400

    stipre webhook debiug

--- apps/payments/views.py
+++ apps/payments/views.py
@@ -16,8 +16,9 @@ stripe.api_key = settings.STRIPE_SECRET_KEY
 
 
 @csrf_exempt
-@require_http_methods(["POST"])
+#@require_http_methods(["POST"])
 def webhook_stripe(request):
+    logger.info(f"Inside stripe")
     payload = request.body
     sig_header = request.META.get('HTTP_STRIPE_SIGNATURE')
     stripe_webhook_secret = settings.STRIPE_WEBHOOK_SECRET
