2034fe5c6426639c0e42e8021091e05dd2d2f1c8
simonfroggatt <simon@safetysignsandnotices.co.uk>
2025-05-07 15:21:57 +0400

    fix for if telephone doesn't exist

--- apps/paperwork/utils.py
+++ apps/paperwork/utils.py
@@ -50,7 +50,8 @@ def create_address(company_obj):
     address += company_obj.company_name
     address += '<BR/>' + company_obj.address
     address += '<BR/>' + company_obj.postcode
-    address += '<BR/>Tel: ' + company_obj.telephone
+    if company_obj.telephone:
+        address += '<BR/>Tel: ' + company_obj.telephone
     address += '<BR/>' + company_obj.email_address
     address += '<BR/>VAT# ' + company_obj.vat_number
     return address
@@ -61,7 +62,8 @@ def contact_details(order_obj):
     if order_obj.shipping_company:
         contacts_str += "Company: " + order_obj.shipping_company + "<BR/>"
     contacts_str += "Contact: " + order_obj.shipping_fullname + "<BR/>"
-    contacts_str += "Telephone: " + order_obj.shipping_telephone
+    if order_obj.shipping_telephone:
+        contacts_str += "Telephone: " + order_obj.shipping_telephone
     return contacts_str
 
 
@@ -102,7 +104,8 @@ def get_shipping_address(order_obj):
 
 def shipping_address_keep(order_obj):
     shipping_str = get_shipping_address(order_obj)
-    shipping_str += "<BR/><BR/>Telephone: " + order_obj.shipping_telephone
+    if order_obj.shipping_telephone:
+        shipping_str += "<BR/><BR/>Telephone: " + order_obj.shipping_telephone
     shipping_str += f'<BR/>Order ref: <b>{order_obj.order_id}</b>'
     shipping_str += "<BR/>Website: " + order_obj.store.name
     return shipping_str
@@ -178,7 +181,8 @@ def order_invoice_details(order_obj):
     if order_obj.customer_order_ref:
         order_str += f'Your Reference: {order_obj.customer_order_ref}<BR/>'
     order_str += f'Main Contact: {order_obj.fullname}<BR/>'
-    order_str += f'Telephone: {order_obj.telephone}<BR/>'
+    if order_obj.telephone:
+        order_str += f'Telephone: {order_obj.telephone}<BR/>'
     order_str += f'Email: {order_obj.email}'
     return order_str
 
@@ -190,7 +194,10 @@ def order_invoice_details_tup(order_obj):
    # order_details_tup['Due Date'] = order_obj.date_due.strftime('%d/%m/%Y')
     order_details_tup['Your Reference:'] = f'{order_obj.customer_order_ref}'
     order_details_tup['Main Contact'] = f'{order_obj.fullname}'
-    order_details_tup['Telephone'] = f'{order_obj.telephone}'
+    if order_obj.telephone:
+        order_details_tup['Telephone'] = f'{order_obj.telephone}'
+    else:
+        order_details_tup['Telephone'] = ''
     order_details_tup['Email'] = f'{order_obj.email}'
     return order_details_tup
 
@@ -249,7 +256,10 @@ def order_proforma_details_tup(order_obj):
     order_details_tup['Proforma Number'] = f'PF-{order_obj.store.prefix}-{order_obj.order_id}'
     order_details_tup['Proforma Date'] = order_obj.date_added.strftime('%d/%m/%Y')
     order_details_tup['Main Contact'] = f'{order_obj.fullname}'
-    order_details_tup['Telephone'] = f'{order_obj.telephone}'
+    if order_obj.telephone:
+        order_details_tup['Telephone'] = f'{order_obj.telephone}'
+    else:
+        order_details_tup['Telephone'] = ''
     order_details_tup['Email'] = f'{order_obj.email}'
     return order_details_tup
 
